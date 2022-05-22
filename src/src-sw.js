d381d94336a3856b5dd38b05e53c97a3d7e2541e
Abass <Abassolanrewaju111@gmail.com>
2022-05-22 11:29:35 +0100

    setup pwa

--- src/src-sw.js
+++ src/src-sw.js
@@ -1,7 +1,4 @@
-/* eslint-disable no-underscore-dangle */
-/* eslint-disable no-restricted-globals */
-// eslint-disable-next-line no-underscore-dangle
-// eslint-disable-next-line no-restricted-globals
-// eslint-disable-next-line no-underscore-dangle
-let workbox;
+import { precacheAndRoute } from 'workbox-precaching';
+
+precacheAndRoute(self.__WB_MANIFEST);
 workbox.precaching.precacheAndRoute(self.__precacheManifest);
