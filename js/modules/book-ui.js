6fbf2823c9973d33ae952072fd117d36676fa85d
Abass Abdul Wasi Olanrewaju <abassolanrewaju111@gmail.com>
2022-05-26 08:57:37 +0100

    Revert "Setup Webpack"

--- js/modules/book-ui.js
+++ js/modules/book-ui.js
@@ -1,4 +1,4 @@
-import { DateTime } from 'luxon';
+import { DateTime } from '../lib/luxon.js';
 
 class BookUi {
   constructor() {
--- src/js/index.js
+++
@@ -1,9 +0,0 @@
-import startApp from './modules/startApp.js';
-
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    navigator.serviceWorker.register('./service-worker.js');
-  });
-}
-
-startApp();
